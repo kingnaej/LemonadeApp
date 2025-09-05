a4930ea90ceda90f6bde14da50f90b63319d6cc8
Naej <naej0767493456@gmail.com>
2025-09-05 20:39:00 +0000

    Mise à jour de l'app Lemonade

--- app/src/main/java/com/example/lemonade/MainActivity.kt
+++ app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -20,12 +20,15 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.lemonade.ui.theme.LemonadeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            LemonApp()
+            LemonadeTheme {
+                LemonApp()
+            }
         }
     }
 }
